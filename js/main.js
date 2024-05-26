600bd7277ef88f4c64eaa6260844968ce4fa7e42
Andrey Rantsev <Rantsev001@gmail.com>
2024-05-26 20:48:41 +0300

    feat: done header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-const burger = document.querySelector('.burger');
+const burger = document.querySelector('#burger');
 burger.addEventListener('click', () => {
-    console.log('Гамбит Пуз');
     burger.classList.toggle('open');
 })
